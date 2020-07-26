8418ea4e0d9e60e48cfb72fba0a8cc282426a839
Steven Sherotsky <ssherotsky@iMac-Pro.local>
2020-07-26 15:32:43 -0700

    consolidate variables

--- aws/standalone_3nic/networking_variables.tf
+++ aws/standalone_3nic/networking_variables.tf
@@ -27,6 +27,58 @@
 #
 ########################################################################################
 
+
+
+########################################################################################
+# AWS Username to prepend all Terraform generated objects -- Your initials work well
+########################################################################################
+
+variable "se_name" {
+  default = "delete-me"
+}
+
+########################################################################################
+# AWS Access key and secret -- Create your access key and secret in the IAM dashboard
+# under your user account
+########################################################################################
+
+variable "aws_access_key" {
+  description = "The AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key"
+}
+
+########################################################################################
+# AWS SSH key pair and public key -- Create a keypair via EC2 dashboard or create your
+# own and upload it via the EC2 dashboard
+########################################################################################
+
+# variable "aws_ssh_key_name" {
+#   description = "SSH key name stored on AWS EC2 to access EC2 instances"
+# }
+
+# variable "aws_ssh_public_key" {
+#   description = "The public part of the SSH key you will use to access EC2 instances"
+# }
+
+# variable "key_name" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.se_name}"
+  public_key = "${tls_private_key.example.public_key_openssh}"
+}
+
+########################################################################################
+# Network -- These should be unique
+########################################################################################
+
 variable "vpc_cidr_block" {
   description = "The CIDR block that will be used for all needed subnets"
   default = "10.0.0.0/16"
@@ -51,3 +103,115 @@ variable "controlling_subnet" {
   description = "The CIDR block of the machines that will SSH into the NSIPs of the VPX HA pair."
   default = "0.0.0.0/0"
   }
+
+########################################################################################
+# AWS Region and Availability Zone
+########################################################################################
+
+variable "aws_region" {
+  description = "The AWS region to create things in"
+  default     = "us-west-2"
+}
+
+variable "aws_availability_zone" {
+  description = "Availability zone to create things in"
+  default     = "us-west-2a"
+}
+
+########################################################################################
+# VPX AMI map -- change these to suit your need
+########################################################################################
+
+variable "vpx_ami_map" {
+  description = <<EOF
+
+AMI map for VPX
+Defaults to VPX BYOL version 13.x
+
+EOF
+
+
+  default = {
+    "us-west-2" = "ami-0a679a9d6bbbd5147"
+    "us-west-1" = "ami-0e608e7c3ca33944b"
+  }
+}
+
+variable "ns_instance_type" {
+  description = <<EOF
+EC2 instance type.
+
+The following values are allowed:
+
+t2.medium
+t2.large
+t2.xlarge
+t2.2xlarge
+m3.large
+m3.xlarge
+m3.2xlarge
+m4.large
+m4.xlarge
+m4.2xlarge
+m4.4xlarge
+m4.10xlarge
+c4.large
+c4.xlarge
+c4.2xlarge
+c4.4xlarge
+c4.8xlarge
+
+EOF
+
+
+default = "m4.xlarge"
+}
+
+########################################################################################
+# ADM Agent AMI map -- change these to suit your need
+########################################################################################
+
+variable "adm_ami_map" {
+  description = <<EOF
+
+AMI map for ADM Agent
+Defaults to ADM Agent version 13.x
+
+EOF
+
+
+  default = {
+    "us-west-2" = "ami-0b30f21b3ccac2121"
+    "us-west-1" = "ami-0b9834f003326ee3c"
+  }
+}
+
+variable "adm_instance_type" {
+  description = <<EOF
+EC2 instance type.
+
+The following values are allowed:
+
+t2.medium
+t2.large
+t2.xlarge
+t2.2xlarge
+m3.large
+m3.xlarge
+m3.2xlarge
+m4.large
+m4.xlarge
+m4.2xlarge
+m4.4xlarge
+m4.10xlarge
+c4.large
+c4.xlarge
+c4.2xlarge
+c4.4xlarge
+c4.8xlarge
+
+EOF
+
+
+default = "m4.xlarge"
+}
